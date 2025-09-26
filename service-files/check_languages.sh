773856aae0cccc52919c57e7c52a2e479469f28c
Børre Gaup <albbas@gmail.com>
2025-09-26 15:58:31 +0200

    Check health of service

--- service-files/check_languages.sh
+++ service-files/check_languages.sh
@@ -2,26 +2,29 @@
 
 port=$1
 if [ -z "$port" ]; then
-    echo "Usage: $0 <port>"
+    echo "Usage: $0 <port> <email>"
     exit 1
 fi
+email=$2
+if [ -z "$email" ]; then
+    echo "Usage: $0 <port> <email>"
+    exit 1
+fi
+
 # Run curl command and capture both output and exit status
 response=$(curl -s "http://localhost:$port/spellcheck/script/ssrv.cgi?cmd=get_lang_list&run_mode=web_service")
 curl_exit_code=$?
 
 # If curl failed, exit with curl's exit status
 if [ $curl_exit_code -ne 0 ]; then
-    echo "curl failed with exit code: $curl_exit_code"
+    mail -s "ck-ospell health check $port failed" $email <<< "Service is stopped, curl failed with exit code: $curl_exit_code"
     exit $curl_exit_code
 fi
 
 # Check if "langList" is in the response
 if echo "$response" | grep -q "langList"; then
-    echo "Success: langList found in response"
-    echo "Response: $response"
     exit 0
 else
-    echo "Error: langList not found in response"
-    echo "Response: $response"
+    mail -s "ck-ospell health check $port failed" $email <<< "Service gives wrong response: expected langList in $response"
     exit 99
 fi
